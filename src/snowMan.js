ff9aed07dc5110025256d5d45df11ca449ae2b17
Adilet312 <adiletm@hotmail.com>
2020-09-04 23:56:30 -0700

    Added self invokation function and declare variabls in order to target elements on the browser.

--- src/snowMan.js
+++ src/snowMan.js
@@ -1,3 +1,14 @@
 import './css/styles.css';
 import generateItems from './generateItems.js';
 generateItems.generateImg;
+let dragndrop = (function(){
+  let positionX = "";
+  let positionY = "";
+  let chooseTarget = "";
+  function moveStart(event){
+    chooseTarget = event.target;
+    positionX = event.offsetX===undefined ? event.layerX : event.offsetX;
+    positionY = event.offsetY===undefined ? event.layery : event.offsetY;
+  }
+  document.querySelector('body').addEventListener('dragstart',false);
+})();
